d78ed427e737475eb4f86164d3ec6afdfc2bf989
krishshah1519 <krishshah1519@gmail.com>
2025-08-20 09:50:43 +0530

    Initial api  setup

--- backend/vault_app/views.py
+++ backend/vault_app/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import Folder, Document, Tag
+from .serializers import FolderSerializer, DocumentSerializer, TagSerializer
+
+class FolderViewSet(viewsets.ModelViewSet):
+    queryset = Folder.objects.all()
+    serializer_class = FolderSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class DocumentViewSet(viewsets.ModelViewSet):
+    queryset = Document.objects.all()
+    serializer_class = DocumentSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class TagViewSet(viewsets.ViewSet):
+
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
