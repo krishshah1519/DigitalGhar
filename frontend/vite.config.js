b8290bb7055c27b05cb8ca963318eb0fd1ea5b6a
krishshah1519 <krishshah1519@gmail.com>
2025-08-20 09:52:54 +0530

    added proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
+// frontend/vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Proxying API requests to the Django backend
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      },
+    },
+  },
 })
