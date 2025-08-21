eae87fac41e55b81d696df4872a30bd0e4c904b8
krishshah1519 <krishshah1519@gmail.com>
2025-08-21 09:25:59 +0530

    feature: jwt authenticationa nd filtering

--- backend/vault_app/urls.py
+++ backend/vault_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import FolderViewSet , DocumentViewSet, TagViewSet
+from .views import RegisterView, FolderViewSet, DocumentViewSet, TagViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = DefaultRouter()
 router.register(r'folders', FolderViewSet)
@@ -8,5 +12,8 @@ router.register(r'documents', DocumentViewSet)
 router.register(r'tags', TagViewSet)
 
 urlpatterns = [
+    path('register/', RegisterView.as_view(), name='auth_register'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include(router.urls)),
 ]
--- backend/vault_app/views.py
+++ backend/vault_app/views.py
@@ -1,7 +1,16 @@
+from rest_framework import viewsets, generics
+from .models import User, Folder, Document, Tag
+from .serializers import RegisterSerializer, FolderSerializer, DocumentSerializer, TagSerializer
+from rest_framework.permissions import AllowAny
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 
-from rest_framework import viewsets
-from .models import Folder, Document, Tag
-from .serializers import FolderSerializer, DocumentSerializer, TagSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+
 
 class FolderViewSet(viewsets.ModelViewSet):
     queryset = Folder.objects.all()
@@ -17,13 +26,18 @@ class DocumentViewSet(viewsets.ModelViewSet):
     queryset = Document.objects.all()
     serializer_class = DocumentSerializer
 
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['folder', 'tags']
+    search_fields = ['name', 'file']
+    ordering_fields = ['name', 'created_at']
+
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-class TagViewSet(viewsets.ViewSet):
+class TagViewSet(viewsets.ModelViewSet):
 
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
