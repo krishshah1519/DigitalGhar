78779af2bfaa84bf453257a088bc46b6776f63d7
krishshah1519 <krishshah1519@gmail.com>
2025-08-21 09:20:16 +0530

    Refactor: Setup Postgresql, added data encryption

--- backend/vault_app/models.py
+++ backend/vault_app/models.py
@@ -1,17 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import secured_fields
+
 
 class User(AbstractUser):
-    dob = models.DateField(null=True, blank=True) # Set to null=True for now
+    first_name = secured_fields.EncryptedCharField(blank=True, max_length=150, verbose_name='first name')
+    last_name = secured_fields.EncryptedCharField(blank=True, max_length=150, verbose_name='last name')
+    email = secured_fields.EncryptedCharField(blank=True, max_length=254, verbose_name='email address')
+    dob = secured_fields.EncryptedDateField(null=True, blank=True)
+    phone_number = secured_fields.EncryptedCharField(max_length=15)
+
     gender_choices = (('male', 'Male'), ('female', 'Female'))
     gender = models.CharField(choices=gender_choices, max_length=10)
-    phone_number = models.CharField(max_length=15)
 
     def __str__(self):
         return self.username
 
 class Folder(models.Model):
-    name = models.CharField(max_length=255)
+    name = secured_fields.EncryptedCharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='folders')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -20,7 +26,7 @@ class Folder(models.Model):
         return self.name
 
 class Tag(models.Model):
-    name = models.CharField(max_length=100)
+    name = secured_fields.EncryptedCharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tags')
 
     class Meta:
@@ -30,8 +36,8 @@ class Tag(models.Model):
         return self.name
 
 class Document(models.Model):
-    name = models.CharField(max_length=255)
-    file = models.FileField(upload_to='documents/')
+    name = secured_fields.EncryptedCharField(max_length=255)
+    file = secured_fields.EncryptedFileField(upload_to='documents/')
     file_type = models.CharField(max_length=50, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='documents')
     folder = models.ForeignKey(Folder, on_delete=models.CASCADE, related_name='documents')
