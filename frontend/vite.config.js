25543a95007f612c6ff22c84f8addd48412afaa3
krishshah1519 <krishshah1519@gmail.com>
2025-08-21 11:04:32 +0530

    Error: Fixed import error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
 // frontend/vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
