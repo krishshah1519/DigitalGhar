f910916478e359f1070a89239713d27bc36c7e51
krishshah1519 <krishshah1519@gmail.com>
2025-08-21 15:19:45 +0530

    corrected a typo

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -6,7 +6,7 @@ const Login = ()=>{
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
-    const Navigate = useNavigate();
+    const navigate = useNavigate();
     
     const handleSubmit =async (e)=> {
         e.preventDefault();
